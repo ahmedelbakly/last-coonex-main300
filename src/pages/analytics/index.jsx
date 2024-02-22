5d29363fd6a210f9f511bbfe753599c4d6f92f5a
ahmed elbakly <78669298+ahmedelbakly@users.noreply.github.com>
2024-02-22 10:20:16 +0200

    analytics

---
+++ src/pages/analytics/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Analytics = () => {
+  return (
+    <div>
+      <h1>Analytics</h1>
+      <p>
+        This page will display any analytics data that is available to the app.
+      </p>
+    </div>
+  );
+};
+
+export default Analytics;
